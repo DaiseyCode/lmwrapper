c081aaed588205050d0e395b421ba170fb1a26b7
David Gros <dngros@live.com>
2023-06-29 14:30:49 -0700

    clean

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def get_requirments():
     import os
     lib_folder = os.path.dirname(os.path.realpath(__file__))
     requirement_path = lib_folder + '/requirements.txt'
-    install_requires = []  # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+    install_requires = []
     if os.path.isfile(requirement_path):
         with open(requirement_path) as f:
             install_requires = f.read().splitlines()
