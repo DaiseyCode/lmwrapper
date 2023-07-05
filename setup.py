91dec3953fbda821891c2bf3631104dd4db242e9
David Gros <dngros@live.com>
2023-07-05 13:10:43 -0700

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirments():
 
 setup(
     name='lmwrapper',
-    version='0.01.00',
+    version='0.01.01',
     author='David Gros',
     description='Wrapper around language model APIs',
     license='MIT',
