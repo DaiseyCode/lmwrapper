8a76a91ea5e069a2348db091637a9c4cd808b9a6
David Gros <dngros@live.com>
2023-08-03 20:56:27 -0700

    weird extra import

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirments():
 
 setup(
     name='lmwrapper',
-    version='0.03.00',
+    version='0.03.01',
     author='David Gros',
     description='Wrapper around language model APIs',
     license='MIT',
