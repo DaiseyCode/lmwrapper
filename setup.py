f95df590c844963d27e6ef6b42d49f8a7d0ea03b
claudiosv <claudiosv@users.noreply.github.com>
2023-08-11 15:45:42 -0700

    Add new requirements

--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
 from setuptools import setup
 
 
-def get_requirments():
-    # https://stackoverflow.com/a/53069528
-    import os
-    lib_folder = os.path.dirname(os.path.realpath(__file__))
-    requirement_path = lib_folder + '/requirements.txt'
-    install_requires = []
-    if os.path.isfile(requirement_path):
-        with open(requirement_path) as f:
-            install_requires = f.read().splitlines()
+def get_requirments(requirements_file: str):
+    from pathlib import Path
+
+    lib_folder = Path(__file__).resolve().parent
+
+    requirement_path = lib_folder / requirements_file
+    if requirement_path.is_file():
+        install_requires = requirement_path.read_text().splitlines()
+
     return install_requires
 
 
 setup(
-    name='lmwrapper',
-    version='0.03.01',
-    author='David Gros',
-    description='Wrapper around language model APIs',
-    license='MIT',
-    packages=['lmwrapper'],
-    install_requires=get_requirments(),
-    python_requires='>=3.10',
+    name="lmwrapper",
+    version="0.03.01",
+    author="David Gros",
+    description="Wrapper around language model APIs",
+    license="MIT",
+    packages=["lmwrapper"],
+    install_requires=get_requirments("requirements.txt"),
+    extras_require={
+        "huggingface": get_requirments("requirements-hf.txt"),
+        "ort": get_requirments("requirements-hf.txt")
+        + get_requirments("requirements-ort.txt")
+        + ["optimum[onnxruntime]>=1.11.0"],
+        "ort-gpu": get_requirments("requirements-hf.txt")
+        + get_requirments("requirements-ort.txt")
+        + ["optimum[onnxruntime-gpu]>=1.11.0"],
+    },
+    python_requires=">=3.10",
 )
