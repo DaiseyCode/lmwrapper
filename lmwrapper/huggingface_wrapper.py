61603b077d415ec571abb401ed428812ba6f0882
claudiosv <claudiosv@users.noreply.github.com>
2023-08-11 15:46:04 -0700

    Add new models, tests, and pytest marker

--- lmwrapper/huggingface_wrapper.py
+++ lmwrapper/huggingface_wrapper.py
@@ -1,24 +1,118 @@
 from dataclasses import dataclass
 from functools import cached_property
+from pathlib import Path
 from typing import Union, List, Any
-
-from transformers.models.auto.tokenization_auto import PreTrainedTokenizerFast
 
 from lmwrapper.abstract_predictor import LmPredictor
 from lmwrapper.structs import LmPrompt, LmPrediction
+from packaging import version
+from importlib.metadata import version as import_version
+from enum import Enum
+import numpy as np
+import os
 
-
-try:
-    from transformers import TextGenerationPipeline, pipeline, GenerationConfig, AutoTokenizer, AutoModel, \
-    AutoModelForCausalLM
-except ImportError:
-    raise ImportError("You must install transformers to use Huggingface models. "
-                      "`pip install transformers` and torch")
+_QUANT_CONFIG = None
+# TODO: Several models do not work on Apple MPS.
+_MPS_ENABLED = os.getenv("MPS_ENABLED", "False").lower() in {"true", "1", "t"}
+_ONNX_RUNTIME = os.getenv("ONNX_RUNTIME", "False").lower() in {"true", "1", "t"}
 
 try:
     import torch
+
+    assert version.parse(torch.__version__) >= version.parse("2.0")
 except ImportError:
-    raise ImportError("Expect to work on torch. Please see https://pytorch.org/ for install info")
+    raise ImportError(
+        "Expect to work on torch. Please see https://pytorch.org/ for install info."
+    )
+
+
+try:
+    import bitsandbytes
+
+    assert version.parse(import_version("bitsandbytes")) >= version.parse("0.41.1")
+
+    from transformers import BitsAndBytesConfig
+
+    _QUANT_CONFIG = BitsAndBytesConfig(
+        # load_in_8bit (bool, optional, defaults to False) — This flag is used to enable 8-bit quantization with LLM.int8().
+        # load_in_4bit (bool, optional, defaults to False) — This flag is used to enable 4-bit quantization by replacing the Linear layers with FP4/NF4 layers from bitsandbytes.
+        # llm_int8_threshold (float, optional, defaults to 6) — This corresponds to the outlier threshold for outlier detection as described in LLM.int8() : 8-bit Matrix Multiplication for Transformers at Scale paper: https://arxiv.org/abs/2208.07339 Any hidden states value that is above this threshold will be considered an outlier and the operation on those values will be done in fp16. Values are usually normally distributed, that is, most values are in the range [-3.5, 3.5], but there are some exceptional systematic outliers that are very differently distributed for large models. These outliers are often in the interval [-60, -6] or [6, 60]. Int8 quantization works well for values of magnitude ~5, but beyond that, there is a significant performance penalty. A good default threshold is 6, but a lower threshold might be needed for more unstable models (small models, fine-tuning).
+        # llm_int8_skip_modules (List[str], optional) — An explicit list of the modules that we do not want to convert in 8-bit. This is useful for models such as Jukebox that has several heads in different places and not necessarily at the last position. For example for CausalLM models, the last lm_head is kept in its original dtype.
+        # llm_int8_enable_fp32_cpu_offload (bool, optional, defaults to False) — This flag is used for advanced use cases and users that are aware of this feature. If you want to split your model in different parts and run some parts in int8 on GPU and some parts in fp32 on CPU, you can use this flag. This is useful for offloading large models such as google/flan-t5-xxl. Note that the int8 operations will not be run on CPU.
+        # llm_int8_has_fp16_weight (bool, optional, defaults to False) — This flag runs LLM.int8() with 16-bit main weights. This is useful for fine-tuning as the weights do not have to be converted back and forth for the backward pass.
+        # bnb_4bit_compute_dtype (torch.dtype or str, optional, defaults to torch.float32) — This sets the computational type which might be different than the input time. For example, inputs might be fp32, but computation can be set to bf16 for speedups.
+        # bnb_4bit_quant_type (str, {fp4, nf4}, defaults to fp4) — This sets the quantization data type in the bnb.nn.Linear4Bit layers. Options are FP4 and NF4 data types which are specified by fp4 or nf4.
+        # bnb_4bit_use_double_quant (bool, optional, defaults to False) — This flag is used for nested quantization where the quantization constants from the first quantization are quantized again.
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+    )
+except ImportError:
+    print("8/4bit quantization is disabled as bitsandbytes could not be loaded.")
+
+try:
+    import transformers
+
+    assert version.parse(transformers.__version__) >= version.parse("4.31.0")
+
+    from transformers import (
+        GenerationConfig,
+        AutoTokenizer,
+        AutoModelForCausalLM,
+        PreTrainedModel,
+        PreTrainedTokenizerFast,
+        set_seed,
+        T5ForConditionalGeneration,
+        AutoModelForSeq2SeqLM,
+        PretrainedConfig,
+    )
+
+    set_seed(42)
+except ImportError:
+    raise ImportError(
+        "You must install torch and transformers to use Huggingface models. "
+        "`pip install transformers` and torch. Please see https://pytorch.org/ for install info."
+    )
+
+if _ONNX_RUNTIME:
+    try:
+        from optimum import version as optimum_version
+
+        assert version.parse(optimum_version.__version__) >= version.parse("1.11.0")
+
+        from optimum.onnxruntime import (
+            ORTModelForCausalLM,
+            ORTModelForSeq2SeqLM,
+            ORTModel,
+            ORTOptimizer,
+        )
+        from optimum.onnxruntime.configuration import AutoOptimizationConfig
+        from optimum.bettertransformer import BetterTransformer
+
+        import xformers
+
+        assert version.parse(xformers.__version__) >= version.parse("0.0.20")
+
+        import onnxruntime
+
+        assert version.parse(onnxruntime.__version__) >= version.parse("1.15.1")
+
+        session_options = onnxruntime.SessionOptions()
+        # session_options.log_severity_level = 0 TODO: set configurable log level
+    except ImportError:
+        raise ImportError(
+            "You must install Optimum, ONNX runtime, and Xformers to use"
+            " accelerated Huggingface models. `pip install"
+            " optimum[onnxruntime-gpu] xformers`"
+        )
+
+
+class Runtime(Enum):
+    PYTORCH = 1
+    ORT_CUDA = 2
+    ORT_TENSORRT = 3
+    ORT_CPU = 4
+    BETTER_TRANSFORMER = 5
+
 
 @dataclass
 class HuggingfacePrediction(LmPrediction):
@@ -34,20 +128,20 @@ class HuggingfacePrediction(LmPrediction):
 
     @property
     def completion_tokens(self) -> List[str]:
-        return self._tokens[self._num_prompt_tokens:]
+        return self._tokens[self._num_prompt_tokens :]
 
     @property
     def completion_logprobs(self) -> List[float]:
         self._verify_logprobs()
-        return self._log_probs[self._num_prompt_tokens:]
+        return self._log_probs[self._num_prompt_tokens :]
 
     @property
     def prompt_tokens(self):
-        return self._tokens[:self._num_prompt_tokens]
+        return self._tokens[: self._num_prompt_tokens]
 
     @property
     def prompt_logprobs(self):
-        return self._log_probs[:self._num_prompt_tokens]
+        return self._log_probs[: self._num_prompt_tokens]
 
     @property
     def full_logprobs(self):
@@ -61,13 +155,17 @@ class HuggingfacePredictor(LmPredictor):
     def __init__(
         self,
         tokenizer: PreTrainedTokenizerFast,
-        model: Any,
+        model: PreTrainedModel,
+        device: torch.device,
     ):
         super().__init__()
         self._tokenizer = tokenizer
         self._model = model
+        self._device = device
 
-    def _predict_maybe_cached(self, prompt: LmPrompt) -> Union[LmPrediction, List[LmPrediction]]:
+    def _predict_maybe_cached(
+        self, prompt: LmPrompt
+    ) -> Union[LmPrediction, List[LmPrediction]]:
         if prompt.stop:
             raise NotImplementedError
         if prompt.presence_penalty:
@@ -76,17 +174,29 @@ class HuggingfacePredictor(LmPredictor):
         if temperature == 0:
             temperature = 1e-9
         assert self._tokenizer.bos_token
+
         encoded_input = self._tokenizer(
-            self._tokenizer.bos_token + prompt.text,
-            return_tensors='pt'
-        )
-        #output = self._model(**encoded_input)
-        #text = self._tokenizer.decode(output[0])
+            self._tokenizer.bos_token + prompt.text, return_tensors="pt"
+        ).to(
+            self._device
+        )  # Move to device
+
+        # ONNX models themselves cannot be moved to a device
+        # but their input tensors must be moved to GPU
+        if not _ONNX_RUNTIME or not isinstance(self._model, ORTModel):
+            self._model.to(self._device)  # Ensure model is on device
+
         # Ref https://gist.github.com/kinoc/8a042d8c5683725aa8c372274c02ea2f
         gen_config = GenerationConfig(
+            max_new_tokens=prompt.max_tokens,
             temperature=temperature,
             top_p=prompt.top_p,
             do_sample=prompt.temperature > 0,
+            return_dict_in_generate=True,
+            output_scores=need_log_prob,
+            pad_token_id=self._tokenizer.pad_token_id,
+            eos_token_id=self._tokenizer.eos_token_id,
+            bos_token_id=self._tokenizer.bos_token_id,
         )
         need_log_prob = prompt.logprobs is not None and prompt.logprobs > 0
 
@@ -105,34 +215,32 @@ class HuggingfacePredictor(LmPredictor):
             val = old_forward(*args, **kwargs)
             cached_logits.append(val.logits)
             return val
+
         self._model.forward = new_call
 
         with torch.no_grad():
             generation_output = self._model.generate(
-                input_ids=encoded_input['input_ids'],
+                **encoded_input,
                 generation_config=gen_config,
-                return_dict_in_generate=True,
-                output_scores=need_log_prob,
-                max_new_tokens=prompt.max_tokens,
-                pad_token_id=self._tokenizer.pad_token_id,
-                eos_token_id=self._tokenizer.eos_token_id,
-                bos_token_id=self._tokenizer.bos_token_id,
             )
 
         self._model.forward = old_forward
 
         s = generation_output.sequences[0]
-        text = self._tokenizer.decode(s[len(encoded_input['input_ids'][0]):])
+        text = self._tokenizer.decode(s[len(encoded_input["input_ids"][0]) :])
         tokens = self._tokenizer.convert_ids_to_tokens(s)
+
         # strip the bos token
         tokens = tokens[1:]
+
         # Calculate the logprobs if needed
         if need_log_prob:
             all_logits = torch.cat(cached_logits, dim=1)
             assert all_logits.shape[0] == 1  # batch
             assert all_logits.shape[1] == len(tokens)
             logprobs = _gather_logprobs_from_logits(
-                all_logits[0], s[1:],
+                all_logits[0],
+                s[1:],
             )
             assert len(logprobs) == len(tokens)
         else:
@@ -151,8 +259,11 @@ class HuggingfacePredictor(LmPredictor):
             metad=generation_output,
             _prompt_encoding=encoded_input,
             _tokens=tokens,
-            _log_probs=logprobs
+            _log_probs=logprobs,
         )
+
+    def get_model_max_length(self) -> int:
+        return int(self._model.config.max_length)
 
     @cached_property
     def space_char(self) -> str:
@@ -179,9 +290,211 @@ def _gather_logprobs_from_logits(
     return gen_probs
 
 
+def _get_accelerator() -> torch.device:
+    if torch.cuda.is_available():
+        if _QUANT_CONFIG:
+            # If quantization is enabled and bits and bytes is not
+            # compiled with CUDA, things don't work right
+            assert bitsandbytes.COMPILED_WITH_CUDA
+        return torch.device("cuda")
+
+    if _MPS_ENABLED and torch.backends.mps.is_available():
+        return torch.device("mps")
+
+    return torch.device("cpu")
+
+
 def get_huggingface_lm(
-    model_name: str
+    model: str,
+    runtime: Runtime = Runtime.PYTORCH,
+    precision: torch.dtype = torch.float32,
+    trust_remote_code: bool = False,
 ) -> HuggingfacePredictor:
+    if runtime != Runtime.PYTORCH:
+        raise Exception(
+            "Accelerated inference model support is still under development."
+            "Please use Runtime.PYTORCH until support matures."
+        )
+
+    _kwargs = {"trust_remote_code": trust_remote_code}
+
+    model_class = AutoModelForCausalLM
+    config_dict = PretrainedConfig.get_config_dict(model)
+
+    has_remote_code = (
+        "auto_map" in config_dict and "AutoConfig" in config_dict["auto_map"]
+    )
+
+    if not trust_remote_code and has_remote_code:
+        raise Exception(
+            "The model provided has remote code and likely will not work as expected. Please call with `trust_remote_code = True`"
+            " If you have read and trust the code."
+        )
+
+    if "auto_map" in config_dict and "AutoModelForSeq2SeqLM" in config_dict["auto_map"]:
+        model_class = AutoModelForSeq2SeqLM
+
+    if model.startswith("Salesforce/codegen"):
+        if runtime == Runtime.BETTER_TRANSFORMER:
+            raise Exception(
+                "WARNING BetterTransformer breaks CodeGen models with AutoClass. Please use a different model or runtime."
+            )
+        else:
+            _kwargs |= {
+                "revision": "main",
+                "use_cache": False,
+            }
+    elif model.startswith("Salesforce/codet5") and not model.endswith("b"):
+        model_class = T5ForConditionalGeneration
+    elif model.startswith("Salesforce/codet5p-") and model.endswith("b"):
+        model_class = AutoModelForSeq2SeqLM
+        _kwargs |= {
+            "low_cpu_mem_usage": True,
+        }
+    elif model == "Salesforce/instructcodet5p-16b":
+        model_class = AutoModelForSeq2SeqLM
+        _kwargs = {
+            "low_cpu_mem_usage": True,
+        }
+
+    return initialize_hf_model(
+        model, model_class, runtime=runtime, precision=precision, _kwargs=_kwargs
+    )
+
+
+def get_ort_model(model: PreTrainedModel) -> ORTModel:
+    if model in {T5ForConditionalGeneration, AutoModelForSeq2SeqLM}:
+        return ORTModelForSeq2SeqLM
+
+    return ORTModelForCausalLM
+
+
+def initialize_hf_model(
+    model_name: str,
+    model_class: PreTrainedModel,
+    runtime: Runtime = Runtime.PYTORCH,
+    precision: torch.dtype = torch.float32,
+    _kwargs: dict = {},
+) -> HuggingfacePredictor:
+    torch_device = _get_accelerator()
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    return HuggingfacePredictor(tokenizer, model)
+
+    if not tokenizer.is_fast:
+        tokenizer = PreTrainedTokenizerFast(tokenizer_object=tokenizer)
+
+    if runtime == Runtime.PYTORCH:
+        model = model_class.from_pretrained(
+            model_name, torch_dtype=precision, device_map="auto", **_kwargs
+        ).to(torch_device)
+    elif runtime == Runtime.ORT_CPU:
+        if not torch_device.type == "cpu":
+            print(
+                f"Specified torch device {torch_device} but ORT CPU runtime"
+                " can only use CPU. Please specify device='cpu'."
+            )
+
+        # ORT models do not support these flags
+        _kwargs.pop("low_cpu_mem_usage", None)
+        _kwargs.pop("device_map", None)
+
+        save_dir = f"{model_name.replace('/','_')}_optimized_cpu_o3"
+
+        if not Path(save_dir).exists():
+            model = get_ort_model(model_class).from_pretrained(
+                model_name,
+                export=True,
+                provider="CPUExecutionProvider",
+                session_options=session_options,
+                **_kwargs,
+            )
+            assert "CPUExecutionProvider" in model.providers
+            optimizer = ORTOptimizer.from_pretrained(model)
+            optimization_config = AutoOptimizationConfig.O3()
+            optimizer.optimize(
+                save_dir=save_dir, optimization_config=optimization_config
+            )
+        model = get_ort_model(model_class).from_pretrained(
+            save_dir, provider="CPUExecutionProvider"
+        )
+    elif runtime == Runtime.ORT_CUDA:
+        if not torch_device.type == "cuda":
+            raise Exception(
+                "Cannot run model on CUDA without CUDA. Please specify device='cuda'."
+            )
+
+        # ORT models do not support these flags
+        _kwargs.pop("low_cpu_mem_usage", None)
+        _kwargs.pop("device_map", None)
+
+        save_dir = f"{model_name.replace('/','_')}_optimized_gpu_o3"
+
+        if not Path(save_dir).exists():
+            model = get_ort_model(model_class).from_pretrained(
+                model_name,
+                export=True,
+                provider="CUDAExecutionProvider",
+                session_options=session_options,
+                **_kwargs,
+            )
+            assert "CUDAExecutionProvider" in model.providers
+            optimizer = ORTOptimizer.from_pretrained(model)
+            optimization_config = AutoOptimizationConfig.O3()
+            optimizer.optimize(
+                save_dir=save_dir, optimization_config=optimization_config
+            )
+        model = get_ort_model(model_class).from_pretrained(
+            save_dir, provider="CUDAExecutionProvider"
+        )
+    elif runtime == Runtime.ORT_TENSORRT:
+        if not torch_device.type == "cuda":
+            raise Exception(
+                "Cannot run model on CUDA without CUDA. Please specify device='cuda'."
+            )
+
+        provider_options = {
+            "trt_engine_cache_enable": True,
+            "trt_engine_cache_path": f"tmp/trt_cache_{model_name.replace('/','_')}_tensorrt",
+        }
+
+        # TensorRT models do not support these flags
+        _kwargs.pop("low_cpu_mem_usage", None)
+        _kwargs.pop("device_map", None)
+
+        model = get_ort_model(model_class).from_pretrained(
+            model_name,
+            export=True,
+            provider="TensorrtExecutionProvider",
+            provider_options=provider_options,
+            session_options=session_options,
+            **_kwargs,
+        )
+        assert "TensorrtExecutionProvider" in model.providers
+    elif runtime == Runtime.BETTER_TRANSFORMER:
+        model = BetterTransformer.transform(
+            model_class.from_pretrained(
+                model_name, device_map="auto", torch_dtype=precision, **_kwargs
+            )
+        )
+    else:
+        raise Exception("Invalid Runtime provided.")
+
+    predictor = HuggingfacePredictor(tokenizer, model, device=torch_device)
+
+    if runtime == Runtime.ORT_TENSORRT:
+        # Warm up TensorRT model once instantiated.
+        print("Warmimg up TensorRT model.")
+        warmup_model(predictor)
+        print("Warmup successful.")
+
+    return predictor
+
+
+def warmup_model(predictor: HuggingfacePredictor):
+    small_prompt = LmPrompt("!", cache=False, temperature=0)
+    predictor.predict(small_prompt)
+
+    long_prompt_str = (
+        "hello world" * predictor.get_model_max_length()
+    )  # TODO: this should be valid model tokens!
+    long_prompt = LmPrompt(long_prompt_str, cache=False, temperature=0)
+    predictor.predict(long_prompt)
---
+++ test/test_huggingface.py
@@ -0,0 +1,87 @@
+import pytest
+
+from lmwrapper.huggingface_wrapper import Runtime, get_huggingface_lm
+from lmwrapper.structs import LmPrompt
+import torch
+from enum import StrEnum
+
+
+class Models(StrEnum):
+    CodeT5plus_220M = "Salesforce/codet5p-220m"
+    CodeT5plus_6B = "Salesforce/codet5p-6b"
+    CodeGen2_1B = "Salesforce/codegen2-1B"
+    CodeGen2_3_7B = "Salesforce/codegen2-3_7B"
+    InstructCodeT5plus_16B = "Salesforce/instructcodet5p-16b"
+
+    DistilGPT2 = "distilgpt2"
+    GPT2 = "gpt2"
+
+
+CUDA_UNAVAILABLE = not torch.cuda.is_available()
+SMALL_GPU = CUDA_UNAVAILABLE or torch.cuda.mem_get_info()[0] < 17_179_869_184  # 16GB
+
+
+if SMALL_GPU:
+    SEQ2SEQ_MODELS = {Models.CodeT5plus_220M}
+    CAUSAL_MODELS = {Models.DistilGPT2, Models.GPT2, Models.CodeGen2_1B}
+else:
+    SEQ2SEQ_MODELS = {Models.CodeT5plus_6B, Models.InstructCodeT5plus_16B}
+    CAUSAL_MODELS = {Models.DistilGPT2, Models.GPT2, Models.CodeGen2_3_7B}
+
+ALL_MODELS = SEQ2SEQ_MODELS | CAUSAL_MODELS
+
+
+def test_distilgpt2_pytorch_runtime():
+    prompt = LmPrompt("print('Hello world", max_tokens=15, cache=False, temperature=0)
+    lm = get_huggingface_lm(lm, runtime=Runtime.PYTORCH)
+    out = lm.predict(prompt)
+    assert out.completion_text
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize("lm", ALL_MODELS)
+def test_all_pytorch_runtime(lm: str):
+    prompt = LmPrompt("print('Hello world", max_tokens=15, cache=False, temperature=0)
+    lm = get_huggingface_lm(lm, runtime=Runtime.PYTORCH)
+    out = lm.predict(prompt)
+    assert out.completion_text
+
+
+@pytest.mark.slow
+@pytest.mark.skip  # ORT is not ready yet
+@pytest.mark.parametrize("runtime", [Runtime.ORT_CPU, Runtime.ORT_CUDA])
+@pytest.mark.parametrize("lm", ALL_MODELS)
+def test_get_ort(runtime: Runtime, lm: str):
+    prompt = LmPrompt("print('Hello world", max_tokens=1, cache=False, temperature=0)
+    lm = get_huggingface_lm(lm, runtime=runtime)
+    out = lm.predict(prompt)
+    assert out.completion_text
+
+
+@pytest.mark.slow
+@pytest.mark.skip  # Better Transformer is not ready yet
+@pytest.mark.parametrize("lm", [Models.DistilGPT2, Models.GPT2])
+def test_get_better_transformer(lm):
+    prompt = LmPrompt("print('Hello world", max_tokens=1, cache=False, temperature=0)
+    lm = get_huggingface_lm(lm, runtime=Runtime.BETTER_TRANSFORMER)
+    out = lm.predict(prompt)
+    assert out.completion_text
+
+
+@pytest.mark.slow
+@pytest.mark.skip  # Better Transformer is not ready yet
+def test_codegen2_predict_bt():
+    lm = Models.CodeGen2_1B
+    with pytest.raises(Exception) as e_info:
+        get_huggingface_lm(lm, runtime=Runtime.BETTER_TRANSFORMER)
+        assert str(e_info.value).startswith("WARNING BetterTransformer")
+
+
+@pytest.mark.slow
+@pytest.mark.skip  # TensorRT is not ready yet
+@pytest.mark.parametrize("lm", CAUSAL_MODELS)
+@pytest.mark.skipif(
+    CUDA_UNAVAILABLE, reason="Cannot test ORT/ONNX CUDA runtime without CUDA"
+)
+def test_get_tensorrt(lm: str):
+    get_huggingface_lm(lm, runtime=Runtime.ORT_TENSORRT)
