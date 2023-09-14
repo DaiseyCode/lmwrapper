7444cd631ca54a65ed2df9ef88f1438012ac517d
DNGros <dngros@live.com>
2023-09-13 23:06:08 -0700

    bump version / force CI rerun

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_readme() -> str:
 
 setup(
     name="lmwrapper",
-    version="0.3.4",
+    version="0.3.5",
     author="David Gros",
     description="Wrapper around language model APIs",
     license="MIT",
