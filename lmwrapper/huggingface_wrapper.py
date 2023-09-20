e522a074e1869bcb3861954de35773c7ebfcdca6
claudiosv <claudiosv@users.noreply.github.com>
2023-09-20 10:57:21 -0700

    Try to fix memory leaks

--- lmwrapper/huggingface_wrapper.py
+++ lmwrapper/huggingface_wrapper.py
@@ -21,6 +21,7 @@ _QUANT_CONFIG = None
 # TODO: Several models do not work on Apple MPS.
 _MPS_ENABLED = os.getenv("MPS_ENABLED", "False").lower() in {"true", "1", "t"}
 _ONNX_RUNTIME = os.getenv("ONNX_RUNTIME", "False").lower() in {"true", "1", "t"}
+_QUANTIZATION_ENABLED = os.getenv("QUANTIZATION", "False").lower() in {"true", "1", "t"}
 
 try:
     import torch
@@ -32,33 +33,33 @@ except ImportError:
         msg,
     )
 
+if _QUANTIZATION_ENABLED:
+    try:
+        import bitsandbytes
 
-try:
-    import bitsandbytes
+        assert version.parse(import_version("bitsandbytes")) >= version.parse(
+            "0.41.1",
+        )
 
-    assert version.parse(import_version("bitsandbytes")) >= version.parse(
-        "0.41.1",
-    )
+        from transformers import BitsAndBytesConfig
 
-    from transformers import BitsAndBytesConfig
-
-    _QUANT_CONFIG = BitsAndBytesConfig(
-        # load_in_8bit (bool, optional, defaults to False) — This flag is used to enable 8-bit quantization with LLM.int8().
-        # load_in_4bit (bool, optional, defaults to False) — This flag is used to enable 4-bit quantization by replacing the Linear layers with FP4/NF4 layers from bitsandbytes.
-        # llm_int8_threshold (float, optional, defaults to 6) — This corresponds to the outlier threshold for outlier detection as described in LLM.int8() : 8-bit Matrix Multiplication for Transformers at Scale paper: https://arxiv.org/abs/2208.07339 Any hidden states value that is above this threshold will be considered an outlier and the operation on those values will be done in fp16. Values are usually normally distributed, that is, most values are in the range [-3.5, 3.5], but there are some exceptional systematic outliers that are very differently distributed for large models. These outliers are often in the interval [-60, -6] or [6, 60]. Int8 quantization works well for values of magnitude ~5, but beyond that, there is a significant performance penalty. A good default threshold is 6, but a lower threshold might be needed for more unstable models (small models, fine-tuning).
-        # llm_int8_skip_modules (List[str], optional) — An explicit list of the modules that we do not want to convert in 8-bit. This is useful for models such as Jukebox that has several heads in different places and not necessarily at the last position. For example for CausalLM models, the last lm_head is kept in its original dtype.
-        # llm_int8_enable_fp32_cpu_offload (bool, optional, defaults to False) — This flag is used for advanced use cases and users that are aware of this feature. If you want to split your model in different parts and run some parts in int8 on GPU and some parts in fp32 on CPU, you can use this flag. This is useful for offloading large models such as google/flan-t5-xxl. Note that the int8 operations will not be run on CPU.
-        # llm_int8_has_fp16_weight (bool, optional, defaults to False) — This flag runs LLM.int8() with 16-bit main weights. This is useful for fine-tuning as the weights do not have to be converted back and forth for the backward pass.
-        # bnb_4bit_compute_dtype (torch.dtype or str, optional, defaults to torch.float32) — This sets the computational type which might be different than the input time. For example, inputs might be fp32, but computation can be set to bf16 for speedups.
-        # bnb_4bit_quant_type (str, {fp4, nf4}, defaults to fp4) — This sets the quantization data type in the bnb.nn.Linear4Bit layers. Options are FP4 and NF4 data types which are specified by fp4 or nf4.
-        # bnb_4bit_use_double_quant (bool, optional, defaults to False) — This flag is used for nested quantization where the quantization constants from the first quantization are quantized again.
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-    )
-except ImportError:
-    logging.warning(
-        "8/4bit quantization is disabled as bitsandbytes could not be loaded.",
-    )
+        _QUANT_CONFIG = BitsAndBytesConfig(
+            # load_in_8bit (bool, optional, defaults to False) — This flag is used to enable 8-bit quantization with LLM.int8().
+            # load_in_4bit (bool, optional, defaults to False) — This flag is used to enable 4-bit quantization by replacing the Linear layers with FP4/NF4 layers from bitsandbytes.
+            # llm_int8_threshold (float, optional, defaults to 6) — This corresponds to the outlier threshold for outlier detection as described in LLM.int8() : 8-bit Matrix Multiplication for Transformers at Scale paper: https://arxiv.org/abs/2208.07339 Any hidden states value that is above this threshold will be considered an outlier and the operation on those values will be done in fp16. Values are usually normally distributed, that is, most values are in the range [-3.5, 3.5], but there are some exceptional systematic outliers that are very differently distributed for large models. These outliers are often in the interval [-60, -6] or [6, 60]. Int8 quantization works well for values of magnitude ~5, but beyond that, there is a significant performance penalty. A good default threshold is 6, but a lower threshold might be needed for more unstable models (small models, fine-tuning).
+            # llm_int8_skip_modules (List[str], optional) — An explicit list of the modules that we do not want to convert in 8-bit. This is useful for models such as Jukebox that has several heads in different places and not necessarily at the last position. For example for CausalLM models, the last lm_head is kept in its original dtype.
+            # llm_int8_enable_fp32_cpu_offload (bool, optional, defaults to False) — This flag is used for advanced use cases and users that are aware of this feature. If you want to split your model in different parts and run some parts in int8 on GPU and some parts in fp32 on CPU, you can use this flag. This is useful for offloading large models such as google/flan-t5-xxl. Note that the int8 operations will not be run on CPU.
+            # llm_int8_has_fp16_weight (bool, optional, defaults to False) — This flag runs LLM.int8() with 16-bit main weights. This is useful for fine-tuning as the weights do not have to be converted back and forth for the backward pass.
+            # bnb_4bit_compute_dtype (torch.dtype or str, optional, defaults to torch.float32) — This sets the computational type which might be different than the input time. For example, inputs might be fp32, but computation can be set to bf16 for speedups.
+            # bnb_4bit_quant_type (str, {fp4, nf4}, defaults to fp4) — This sets the quantization data type in the bnb.nn.Linear4Bit layers. Options are FP4 and NF4 data types which are specified by fp4 or nf4.
+            # bnb_4bit_use_double_quant (bool, optional, defaults to False) — This flag is used for nested quantization where the quantization constants from the first quantization are quantized again.
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=True,
+        )
+    except ImportError:
+        logging.warning(
+            "8/4bit quantization is disabled as bitsandbytes could not be loaded.",
+        )
 
 try:
     import transformers
@@ -266,35 +267,56 @@ class HuggingfacePredictor(LmPredictor):
         log_cuda_mem()
         need_log_prob = prompt.logprobs is not None and prompt.logprobs > 0
 
-        # Some models do not have a pad token, default to 0
-        if self._tokenizer.pad_token_id:
-            pad_token_id = self._tokenizer.pad_token_id
-
-        else:
-            pad_token_id = 0
-            logging.warning(
-                "Tokenizer does not have a pad_token_id. Setting pad_token_id to 0. May cause unexpected behavior."
-            )
-
         # UserWarning: `do_sample` is set to `False`.
         # However, `temperature` is set to `0.0` -- this flag is only used in
         # sample-based generation modes. You should set `do_sample=True` or unset
         # `temperature`. This was detected when initializing the generation config
         # instance, which means the corresponding file may hold incorrect
         # parameterization and should be fixed.
+        do_sample = prompt.temperature != 1.0
+        num_beams = 1
+        penalty_alpha = 0.
+        top_k = 50
+        num_beam_groups = 1
         generation_kwargs = {
             "temperature": prompt.temperature,
             # "top_p": prompt.top_p,
-            # "do_sample": prompt.temperature > 0,
+            "do_sample": do_sample,
+            # "diversity_penalty": prompt.presence_penalty, # This value is subtracted from a beam’s score
+            # if it generates a token same as any beam from other group at a particular time.
+            # Note that diversity_penalty is only effective if group beam search is enabled.
+            # "repetition_penalty": prompt.frequency_penalty # The parameter for repetition penalty. 1.0 means no penalty
         }
-        if prompt.temperature == 0.0:
+        if prompt.temperature == 1.0:
             generation_kwargs.pop("temperature", None)
+            generation_kwargs.pop("do_sample", None)
+
+        # num_beams (int, optional, defaults to 1) — Number of beams for beam search. 1 means no beam search.
+        if num_beams==1 and do_sample==False:
+            logging.info("Decoding strategy: greedy decoding")
+        elif penalty_alpha>0. and top_k>1:
+            logging.info("Decoding strategy: contrastive search")
+        elif num_beams==1 and do_sample==True:
+            logging.info("Decoding strategy: multinomial sampling")
+        elif num_beams>1 and do_sample==False:
+            logging.info("Decoding strategy: beam-search decoding")
+        elif num_beams>1 and do_sample==True:
+            logging.info("Decoding strategy: beam-search multinomial sampling")
+        elif num_beams>1 and num_beam_groups>1:
+            logging.info("Decoding strategy: diverse beam-search")
+        else:
+            logging.info("Unable to predict decoding strategy!")
+        # elif constraints!=None or force_words_ids!=None:
+        #     logging.info("constrained beam-search")
+        # elif assistant_model is passed to .generate():
+        #     logging.info("assisted decoding")
+
         # Ref https://gist.github.com/kinoc/8a042d8c5683725aa8c372274c02ea2f
         gen_config = GenerationConfig(
             max_new_tokens=prompt.max_tokens,
             return_dict_in_generate=True,
             output_scores=need_log_prob,
-            pad_token_id=pad_token_id,
+            pad_token_id=self._tokenizer.pad_token_id,
             eos_token_id=self._tokenizer.eos_token_id,
             bos_token_id=self._tokenizer.bos_token_id,
             **generation_kwargs,
@@ -336,6 +358,7 @@ class HuggingfacePredictor(LmPredictor):
                 generation_config=gen_config,
                 stopping_criteria=stopping_criteria,
             )
+        logging.info("Generation output type:" + str(type(generation_output)))
         print("Post generate")
         log_cuda_mem()
 
@@ -446,18 +469,22 @@ class HuggingfacePredictor(LmPredictor):
         print("Pre del statements")
         log_cuda_mem()
         np_logprobs = logprobs.detach().cpu().numpy()
-        np_encoded_input = encoded_input.to("cpu").convert_to_tensors(TensorType.NUMPY).copy()
+        np_encoded_input = (
+            encoded_input.to("cpu").convert_to_tensors(TensorType.NUMPY).copy()
+        )
 
         # generation_output needs to be mapped to .detach().cpu().numpy() for all tensors
         updated_output = {}
 
+        def numpy_tuple(value):
+            if isinstance(value, torch.Tensor):
+                return value.detach().cpu().numpy()
+            if isinstance(value, tuple):
+                return tuple(numpy_tuple(v) for v in value)
+
         for key, value in generation_output.items():
-            if (
-                isinstance(value, torch.Tensor) and value.nelement() > 0
-            ):  # Check if tensor and not empty
-                updated_output[key] = value.detach().cpu().numpy()
-            else:
-                updated_output[key] = value
+            updated_output[key] = numpy_tuple(value)
+
         del generation_output
         del logprobs
         del encoded_input
@@ -756,6 +783,13 @@ def _initialize_hf_model(
     else:
         msg = "Invalid Runtime provided."
         raise Exception(msg)
+
+    # Some models do not have a pad token, default to 0
+    if not tokenizer.pad_token_id:
+        tokenizer.pad_token_id = 0
+        logging.warning(
+            "Tokenizer does not have a pad_token_id. Setting pad_token_id to 0. May cause unexpected behavior."
+        )
 
     predictor = HuggingfacePredictor(
         tokenizer,
