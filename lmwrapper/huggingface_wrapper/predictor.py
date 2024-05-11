make the internals arg for prediction not required. Fix possible bug with echo full logprobs

lmwrapper/huggingface_wrapper/predictor.py
@@ -253,23 +253,15 @@ class HuggingFacePredictor(LmPredictor):
             #   require calling the model again https://discuss.huggingface.co/t/announcement-generation-get-probabilities-for-generated-output/30075/17
             # Instead we are going to patch the model forward to log calls
             old_forward = self._model.forward
-            cached_logits = torch.zeros(0)
+            cached_logits = []
 
-            if model_requires_attention_mask:
-
-                def new_call(attention_mask, *args, **kwargs):
-                    nonlocal cached_logits
-                    val = old_forward(attention_mask=attention_mask, *args, **kwargs)
-                    cached_logits = val.logits
-                    return val
-
-            else:
-
-                def new_call(*args, **kwargs):
-                    nonlocal cached_logits
-                    val = old_forward(*args, **kwargs)
-                    cached_logits = val.logits
-                    return val
+            def new_call(attention_mask, *args, **kwargs):
+                nonlocal cached_logits
+                if model_requires_attention_mask:
+                    kwargs["attention_mask"] = attention_mask
+                val = old_forward(*args, **kwargs)
+                cached_logits.append(val.logits.detach())
+                return val
 
             self._model.forward = new_call
 
@@ -392,10 +384,20 @@ class HuggingFacePredictor(LmPredictor):
                 assert prompt.echo
                 assert not self.is_encoder_decoder
 
-                assert cached_logits.shape[0] == 1  # batch
-                assert cached_logits.shape[1] == len(model_output_sequence[1:])
+                assert cached_logits[0].shape[0] == 1  # batch
+                if len(cached_logits) > 1 and cached_logits[-1].shape[1] == 1:
+                    combined_logits = torch.cat(cached_logits, dim=1)
+                else:
+                    combined_logits = cached_logits[-1]
+                if not (combined_logits.shape[1] == len(model_output_sequence[1:])):
+                    msg = (
+                        f"Logits shape does not match the output sequence length\n"
+                        f"Logits shape: {cached_logits.shape}\n"
+                        f"Output sequence length: {len(model_output_sequence[1:])}"
+                    )
+                    raise RuntimeError(msg)
                 output_logprobs = _gather_logprobs_from_logits(
-                    cached_logits[0],
+                    combined_logits[0],
                     model_output_sequence[1:],
                 )
 
@@ -404,6 +406,7 @@ class HuggingFacePredictor(LmPredictor):
                 # Free memory
                 del output_logprobs
                 del cached_logits
+                del combined_logits
 
                 assert len(model_output_sequence[1:]) == len(logprobs)
                 if stop_token_idx_output and stop_token_idx_output > 0:

test/test_huggingface_internals.py
@@ -46,49 +46,53 @@ def test_get_internals_hidden_states(pytestconfig, model_name_layers_hidden):
     num_expected_tokens = len(prompt_tokens) + num_gen_tokens
     assert num_expected_tokens == 12
     pred = model.predict(prompt)
-    assert pred.internals.hidden_states is not None
-    assert isinstance(pred.internals.hidden_states, tuple)
-    assert len(pred.internals.hidden_states) == 1 + num_layers  # has embedding layer
-    assert all(isinstance(x, np.ndarray) for x in pred.internals.hidden_states)
-    assert all(
-        x.shape == (num_expected_tokens, hidden_size)
-        for x in pred.internals.hidden_states
-    )
-    assert pred.internals.attentions is None
-    print(pred)
 
-    # Try with a selection of indexes
-    prompt = dataclasses.replace(
-        prompt,
-        model_internals_request=ModelInternalsRequest(
-            return_hidden_states=True,
-            hidden_layer_indexes=[0, 5, -1],
-        ),
-    )
-    new_pred = model.predict(prompt)
-    new_hidden = new_pred.internals.hidden_states
-    assert len(new_hidden) == 3
-    assert np.allclose(new_hidden[0], pred.internals.hidden_states[0])
-    assert np.allclose(new_hidden[1], pred.internals.hidden_states[5])
-    assert np.allclose(new_hidden[2], pred.internals.hidden_states[-1])
+    def check_vals(pred, prompt):
+        assert pred.internals.hidden_states is not None
+        assert isinstance(pred.internals.hidden_states, tuple)
+        assert len(pred.internals.hidden_states) == 1 + num_layers  # has embedding layer
+        assert all(isinstance(x, np.ndarray) for x in pred.internals.hidden_states)
+        assert all(
+            x.shape == (num_expected_tokens, hidden_size)
+            for x in pred.internals.hidden_states
+        )
+        assert pred.internals.attentions is None
+        print(pred)
 
-    # Try with fracs
-    prompt = dataclasses.replace(
-        prompt,
-        model_internals_request=ModelInternalsRequest(
-            return_hidden_states=True,
-            hidden_layer_fractions=[0.0, 0.5, 1.0],
-        ),
-    )
-    new_pred = model.predict(prompt)
-    new_hidden = new_pred.internals.hidden_states
-    assert len(new_hidden) == 3
-    assert np.allclose(new_hidden[0], pred.internals.hidden_states[0])
-    assert np.allclose(
-        new_hidden[1],
-        pred.internals.hidden_states[round((1 + num_layers) / 2)],
-    )
-    assert np.allclose(new_hidden[2], pred.internals.hidden_states[-1])
+        # Try with a selection of indexes
+        prompt = dataclasses.replace(
+            prompt,
+            model_internals_request=ModelInternalsRequest(
+                return_hidden_states=True,
+                hidden_layer_indexes=[0, 5, -1],
+            ),
+        )
+        new_pred = model.predict(prompt)
+        new_hidden = new_pred.internals.hidden_states
+        assert len(new_hidden) == 3
+        assert np.allclose(new_hidden[0], pred.internals.hidden_states[0])
+        assert np.allclose(new_hidden[1], pred.internals.hidden_states[5])
+        assert np.allclose(new_hidden[2], pred.internals.hidden_states[-1])
+
+        # Try with fracs
+        prompt = dataclasses.replace(
+            prompt,
+            model_internals_request=ModelInternalsRequest(
+                return_hidden_states=True,
+                hidden_layer_fractions=[0.0, 0.5, 1.0],
+            ),
+        )
+        new_pred = model.predict(prompt)
+        new_hidden = new_pred.internals.hidden_states
+        assert len(new_hidden) == 3
+        assert np.allclose(new_hidden[0], pred.internals.hidden_states[0])
+        assert np.allclose(
+            new_hidden[1],
+            pred.internals.hidden_states[round((1 + num_layers) / 2)],
+        )
+        assert np.allclose(new_hidden[2], pred.internals.hidden_states[-1])
+
+    check_vals(pred, prompt)
 
     ### Attentions
     # (we are packing a lot into one test to still parameterize but no reload model)
@@ -128,3 +132,36 @@ def test_get_internals_hidden_states(pytestconfig, model_name_layers_hidden):
     assert np.allclose(pred_sub.internals.attentions[0], pred.internals.attentions[0])
     assert np.allclose(pred_sub.internals.attentions[1], pred.internals.attentions[5])
     assert np.allclose(pred_sub.internals.attentions[2], pred.internals.attentions[-1])
+
+    ### With logprobs
+    prompt = LmPrompt(
+        text="The capital Germany is Berlin. The capital of France",
+        max_tokens=num_gen_tokens,
+        temperature=0,
+        echo=True,
+        logprobs=1,
+        model_internals_request=ModelInternalsRequest(
+            return_hidden_states=True,
+        ),
+    )
+    prompt_tokens = model.tokenize(prompt.text)
+    num_expected_tokens = len(prompt_tokens) + num_gen_tokens
+    assert num_expected_tokens == 12
+    pred = model.predict(prompt)
+    check_vals(pred, prompt)
+    assert len(pred.full_logprobs) == num_expected_tokens
+    assert len(pred.completion_logprobs) == num_gen_tokens
+
+    prompt = LmPrompt(
+        text="The capital Germany is Berlin. The capital of France",
+        max_tokens=num_gen_tokens,
+        temperature=0,
+        echo=False,
+        logprobs=1,
+        model_internals_request=ModelInternalsRequest(
+            return_hidden_states=True,
+        ),
+    )
+    pred = model.predict(prompt)
+    check_vals(pred, prompt)
+    assert len(pred.completion_logprobs) == num_gen_tokens